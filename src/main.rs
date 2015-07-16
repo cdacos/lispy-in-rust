63e59b00eb8b813a3d390d303ab261ef8bdd7b9b
Carlos da Costa <carlos@kaldorgroup.com>
2015-07-16 16:53:09 +0100

    Second attempt. Language 1 (Lispy Calculator) is mostly working.

--- src/main.rs
+++ src/main.rs
@@ -1,180 +1,280 @@
-use std::collections::HashMap;
+// See http://norvig.com/lispy.html
 
 #[derive(Debug)]
 struct AstNode {
+    list: Vec<AstNode>,
     symbol: Option<String>,
-    list: Vec<Box<AstNode>>,
     number: Option<f32>,
 }
 
 impl AstNode {
     fn new() -> AstNode {
         AstNode {
-            symbol: None,
             list: vec![],
+            symbol: None,
             number: None,
         }
+    } 
+
+    fn clone(&self) -> AstNode {
+        let mut node = AstNode::new();
+        node.number = self.number;
+        match self.symbol {
+            Some(ref s)  => node.symbol = Some(s.clone()),
+            None => node.symbol = None,
+        }
+        node
     }
 }
 
-fn tokenise(s: &str) -> Vec<String> {
-    let spaced = s.replace("(", " ( ").replace(")", " ) ");
-    spaced.split_whitespace()
-        .filter(|&x| x.trim() != "")
+//Convert a string of characters into a list of tokens.
+fn tokenize(s: &str) -> Vec<String> {
+    s.replace("(", " ( ")
+        .replace(")", " ) ")
+        .split_whitespace()
         .map(|x| x.to_string())
         .collect()
 }
 
-fn atom(node: &mut AstNode, token: &str) {
-    match token.parse::<f32>() {
-        Ok(f) => node.number = Some(f),
-        Err(_) => node.symbol = Some(token.to_string()),
-    }
+// Read a Scheme expression from a string.
+fn parse(program: &str) -> AstNode {
+    read_from_tokens(&mut tokenize(program))
 }
 
-fn read_from_tokens(tokens: &mut Vec<String>) -> Box<AstNode> {
-    let mut node = AstNode::new();
+// Read an expression from a sequence of tokens.
+fn read_from_tokens(tokens: &mut Vec<String>) -> AstNode {
     let token = tokens.remove(0);
-    match token.as_ref() {
-        "(" => {            
+    match &*token {
+        "(" => {
+            let mut atom = AstNode::new();
             while tokens[0] != ")" {
-                node.list.push(read_from_tokens(tokens));
+                atom.list.push(read_from_tokens(tokens));
             }
-            tokens.remove(0); // remove ")"
+            tokens.remove(0); // pop off ')'
+            atom
         },
         ")" => panic!("unexpected token"),
-        _ => {
-            atom(&mut node, token.as_ref());
-        }
-    };
-    Box::new(node)
-}
-
-#[allow(dead_code)]
-fn print_ast(node: &AstNode, depth: i32) {
-    let spaces = (1..depth).map(|_| " ").collect::<String>();
-    let symbol = node.symbol.clone();
-
-    match node.number {
-        Some(f) => println!("{}{}", spaces, f),
-        None => (),
-    }
-
-    match symbol {
-        Some(s) => println!("{}{}", spaces, s),
-        None => (),
-    }
-
-    if node.list.len() > 0 {
-        println!("{}(", spaces);
-        for child in &node.list {
-            print_ast(&child, depth + 1);
-        }
-        println!("{})", spaces);
+        _ => atom(token)
     }
 }
 
-struct EnvObj {
-    f0: Option<fn() -> f32>,
-    f1: Option<fn(x: f32) -> f32>,
-    f2: Option<fn(x: f32, y: f32) -> f32>,
+// Numbers become numbers; every other token is a symbol.
+fn atom(token: String) -> AstNode {
+    let mut atom = AstNode::new();
+    match token.parse::<f32>() {
+        Ok(v) => atom.number = Some(v),
+        _ => atom.symbol = Some(token),
+    }
+    atom
+}
+
+// -----
+use std::collections::HashMap;
+use std::f32::consts::PI;
+use std::fmt;
+
+struct Value {
+    function: Option<fn(x: Vec<Value>) -> Value>,
     number: Option<f32>,
+    //TODO string: Option<String>,
 }
 
-impl EnvObj {
-    fn new() -> EnvObj {
-        EnvObj {
-            f0: None,
-            f1: None,
-            f2: None,
-            number: None,
+impl fmt::Debug for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.number != None {
+            write!(f, "Value: {}", self.number.unwrap())
+        }
+        else {
+            write!(f, "Value: Function")
         }
     }
+}
 
-    #[allow(dead_code)]
-    fn f0(f: fn() -> f32) -> EnvObj {
-        let mut env_obj = EnvObj::new();
-        env_obj.f0 = Some(f);
-        env_obj
+impl Value {
+    fn new(f: Option<fn(x: Vec<Value>) -> Value>, n: Option<f32>) -> Value {
+        Value {
+            function: f,
+            number: n,
+        }
+    }
+    
+    fn function(f: fn(x: Vec<Value>) -> Value) -> Value {
+        Value::new(Some(f), None)
+    }
+    
+    fn number(n: f32) -> Value {
+        Value::new(None, Some(n))
     }
 
-    #[allow(dead_code)]
-    fn f1(f: fn(x: f32) -> f32) -> EnvObj {
-        let mut env_obj = EnvObj::new();
-        env_obj.f1 = Some(f);
-        env_obj
-    }
-
-    fn f2(f: fn(x: f32, y: f32) -> f32) -> EnvObj {
-        let mut env_obj = EnvObj::new();
-        env_obj.f2 = Some(f);
-        env_obj
-    }
-
-    fn n(n: f32) -> EnvObj {
-        let mut env_obj = EnvObj::new();
-        env_obj.number = Some(n);
-        env_obj
-    }
-
-    fn clone(&self) -> EnvObj {
-        let mut env_obj = EnvObj::new();
-        env_obj.f0 = self.f0;
-        env_obj.f1 = self.f1;
-        env_obj.f2 = self.f2;
-        env_obj.number = self.number;
-        env_obj
+    fn clone(&self) -> Value {
+        Value::new(self.function, self.number)
     }
 }
 
-fn add(x: f32, y: f32) -> f32 {
-    x + y
-}
-
-fn multiply(x: f32, y: f32) -> f32 {
-    x * y
-}
-
-fn standard_env<'a>() -> HashMap<&'a str, EnvObj> {
-    let mut env: HashMap<&'a str, EnvObj> = HashMap::new();
-    env.insert("pi", EnvObj::n(3.1415926535f32));
-    env.insert("+", EnvObj::f2(add));
-    env.insert("*", EnvObj::f2(multiply));
+// An environment with some Scheme standard procedures.
+fn standard_env() -> HashMap<String, Value> {
+    let mut env: HashMap<String, Value> = HashMap::new();
+    env.insert(string_from_str("pi"), Value::number(PI));
+    env.insert(string_from_str("+"), Value::function(add));
+    env.insert(string_from_str("-"), Value::function(subtract));
+    env.insert(string_from_str("*"), Value::function(multiply));
+    env.insert(string_from_str("/"), Value::function(divide));
     env
 }
 
-fn eval(x: AstNode, env: &HashMap<&str, EnvObj>) -> EnvObj {
-    if x.symbol != None {
-        let s: &str = &*x.symbol.unwrap();
+fn string_from_str(string: &str) -> String {
+    let mut s = String::new();
+    s.push_str(string);
+    s
+}
+
+// -----
+
+
+fn add(args: Vec<Value>) -> Value {
+    let mut result = 0f32;
+
+    match args.len() {
+        0 => panic!("Add needs at least one number"),
+        _ => for arg in args {
+                result += arg.number.unwrap();
+        }
+    }
+    
+    Value::number(result)
+}
+
+fn subtract(args: Vec<Value>) -> Value {
+    let mut result = 0f32;
+
+    match args.len() {
+        0 => panic!("Subtract needs at least one number"),
+        _ => for arg in args {
+                result -= arg.number.unwrap();
+        }
+    }
+    
+    Value::number(result)
+}
+
+fn multiply(args: Vec<Value>) -> Value {
+    let mut result = 1f32;
+
+    match args.len() {
+        0 => panic!("Multiply needs at least one number"),
+        _ => for arg in args {
+                result *= arg.number.unwrap();
+        }
+    }
+    
+    Value::number(result)
+}
+
+fn divide(args: Vec<Value>) -> Value {
+    let mut result;
+
+    match args.len() {
+        0 => panic!("Divide needs at least one number"),
+        _ => {
+            result = args[0].number.unwrap();
+            for i in 1..args.len() {
+                result /= args[i].number.unwrap();
+            }
+        }
+    }
+    
+    Value::number(result)
+}
+
+// -----
+
+// Evaluate an expression in an environment.
+fn eval(atom: AstNode, env: &mut HashMap<String, Value>) -> Value {
+    if atom.symbol != None { // variable reference
+        let s: &str = &*atom.symbol.unwrap();
         env.get(s).unwrap().clone()
     }
-    else if x.number != None {
-        EnvObj::n(x.number.unwrap())
+    else if atom.number != None { // constant literal
+        Value::number(atom.number.unwrap())
     }
-    else {
-        let mut args: Vec<Box<AstNode>> = x.list;
-        let node: AstNode = *args.remove(0);
-        let f = eval(node, env);
-        let mut results: Vec<EnvObj> = vec![];
-        for arg in args {
-            results.push(eval(*arg, &env));
+    else if atom.list.len() > 0 {  // non-empty list
+        let mut list = atom.list;
+        let first: AstNode = list.remove(0);
+        let first_symbol = first.clone().symbol;
+        
+        if first_symbol != None {
+            match &*first_symbol.unwrap() {
+                "quote" =>  { // (quote exp)
+                    panic!("TODO: Handle quote");
+                },
+                "if" => { // (if test conseq alt)
+                    if list.len() != 3 {
+                        panic!("If must be in the form of: (if test conseq alt)");
+                    }
+                    
+                    let test = list.remove(0);
+                    let conseq = list.remove(0);
+                    let alt = list.remove(0);
+                    let test_result = eval(test, env).number.unwrap();
+                    
+                    return eval(if test_result != 0f32 { conseq } else { alt }, env);
+                },
+                "define" => { // (define var exp)
+                    match list.remove(0).symbol {
+                        Some(v) => {
+                            let exp = eval(list.remove(0), env);
+                            env.insert(v.clone(), exp);
+                            return Value::new(None, None);
+                        },
+                        None => panic!("Define must be in the form: (define var exp)"),
+                    }
+                },
+                _ => {}
+            }
         }
-        match results.len() {
-            0 => EnvObj::n(f.f0.unwrap()()),
-            1 => EnvObj::n(f.f1.unwrap()(results[0].number.unwrap())),
-            _ => EnvObj::n(f.f2.unwrap()(results[0].number.unwrap(), results[1].number.unwrap())),
+
+        //  (proc arg...)
+        let op = eval(first, env).function.unwrap();
+        let mut args: Vec<Value> = vec![];
+        for arg in list {
+            args.push(eval(arg, env));
         }
+        op(args)
+    }
+    else { // empty list
+        panic!("TODO: Handle empty lists");
     }
 }
 
+use std::io;
+
+// A prompt-read-eval-print loop.
+fn repl(prompt: &str, env: &mut HashMap<String, Value>) {
+    loop {
+        println!("{}", prompt);
+
+        let mut expression = String::new();
+
+        io::stdin().read_line(&mut expression)
+            .ok()
+            .expect("failed to read line");
+
+        let ast = parse(&*expression);
+        let t = eval(ast, env);
+
+        if t.number != None {
+            println!("{:#?}", t.number.unwrap());
+        }
+//        else {
+//            println!("\n>>> No result returned");
+//            println!("\n>>> The environment is: {:#?}", env);
+//        }
+    }
+}
+
+// -----
+
 fn main() {
-    //let expression = "(begin (define r 10) (* pi (* r r)))";
-    let expression = "(* 2 (+ 3 4))";
-    let ast = read_from_tokens(&mut tokenise(expression));
-
-    //print_ast(&ast, 1);
-    //println!("{:#?}", &ast); // Nice pretty debug print
-
-    let result = eval(*ast, &standard_env());
-    println!("{} = {:#?}", expression, result.number.unwrap());
+    let mut env = standard_env();
+    repl("rust-lispy> ", &mut env);
 }
